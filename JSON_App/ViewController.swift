81017b3feb1af57ef059ea5bad9ddf0f85ad1d05
Ula <1295593@mail.ru>
2022-02-24 16:43:57 +0800

    Initial Commit

---
+++ JSON_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSON_App
+//
+//  Created by Юлия Алдохина on 24/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
